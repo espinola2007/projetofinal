a998acd3f44c151936fb7ecdbc2df4aef62e6dbe
espinola2007 <espinolavitoria3@gmail.com>
2025-11-18 19:52:40 -0300

    criação do template

---
+++ wepink/includes/footer.php
@@ -0,0 +1,7 @@
+</main>
+<footer>
+  <hr>
+  <p>© 2025 WePink - Todos os direitos reservados.</p>
+</footer>
+</body>
+</html>
---
+++ wepink/index.php
@@ -0,0 +1,9 @@
+<?php include 'includes/header.php'; ?>
+
+<section class="banner">
+  <h2>Beleza que insPIRA!</h2>
+  <p>Conheça a linha de cosméticos WePink.</p>
+  <a href="produtos.php" class="btn">Ver Produtos</a>
+</section>
+  
+<?php include 'includes/footer.php'; ?>
