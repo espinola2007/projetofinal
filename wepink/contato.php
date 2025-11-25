02f6ae8c79d4585ae841ed59ad2a0434125c6050
Hellen2704 <hellen.brayner@outlook.com>
2025-11-25 20:44:44 -0300

    TELEFONE

--- wepink/contato.php
+++ wepink/contato.php
@@ -1,1 +1,11 @@
+<?php
 
+$email = "wepink@empresa.com";
+$telefone = "(83) 8002-8922";
+?>
+
+<label>E-mail da empresa:</label>
+<input type="text" value="<?php echo $email; ?>" disabled><br><br>
+
+<label>Telefone:</label>
+<input type="text" value="<?php echo $telefone; ?>" disabled>
