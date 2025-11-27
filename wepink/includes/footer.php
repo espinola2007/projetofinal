d97aee6a4fc8736e8a2408803442d686f644007b
espinola2007 <130624736+espinola2007@users.noreply.github.com>
2025-11-26 21:51:41 -0300

    Delete wepink directory

--- wepink/includes/footer.php
+++
@@ -1,7 +0,0 @@
-</main>
-<footer>
-  <hr>
-  <p>© 2025 WePink - Todos os direitos reservados.</p>
-</footer>
-</body>
-</html>
--- wepink/sobre.php
+++
@@ -1,5 +0,0 @@
-<?php include 'includes/header.php'; ?>
-<h2>Sobre a WePink</h2>
-<p>A WePink é uma marca de cosméticos que acredita na beleza natural e no autocuidado.</p>
-<?php include 'includes/footer.php'; ?>
-
